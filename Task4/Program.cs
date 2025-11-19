8ceab49bd31a8a3e5a214cbe83f9c602ce2b5593
Mukunda-Katel <mukundakatel@gmail.com>
2025-11-19 22:05:06 +0545

    task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            Player p1 = new Player();
+            p1.playerName = "Bipul Bhandari";
+            p1.level = 0;
+            p1.health = 0;
+
+            Player p2 = new Player("Mukunda Katel", 5, 100);
+
+            Console.WriteLine("\nPlayer 1 (Default Constructor)");
+            Console.WriteLine("Name: " + p1.playerName);
+            Console.WriteLine("Level: " + p1.level);
+            Console.WriteLine("Health: " + p1.health);
+
+            Console.WriteLine("\nPlayer 2 (Parameterized Constructor)");
+            Console.WriteLine("Name: " + p2.playerName);
+            Console.WriteLine("Level: " + p2.level);
+            Console.WriteLine("Health: " + p2.health);
+
+            Console.ReadLine();
+        }
+    }
+}
