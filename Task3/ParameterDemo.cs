8dbf9d0d9d3e17c9c32853410c37bb8fd71d7046
Mukunda-Katel <mukundakatel@gmail.com>
2025-11-19 21:49:50 +0545

    task 3

---
+++ Task3/ParameterDemo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class ParameterDemo
+    {
+        public void Increase(ref int number)
+        {
+            number += 10;
+        }
+
+        public void GetFullName(out string fullname)
+        {
+            fullname = "Mukunda Katel";
+        }
+
+
+        public int SumAll(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            ParameterDemo demo = new ParameterDemo();
+
+            int value = 20;
+            Console.WriteLine("Before Increase: " + value);
+            demo.Increase(ref value);
+            Console.WriteLine("After Increase: " + value);
+
+            demo.GetFullName(out string fullName);
+            Console.WriteLine("Full Name: " + fullName);
+            int total = demo.SumAll(5, 10, 15, 20);
+            Console.WriteLine("Sum of all numbers: " + total);
+
+            Console.ReadLine();
+        }
+    }
+}
