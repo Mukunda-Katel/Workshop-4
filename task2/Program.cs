c8ff667b095bf1578476a6a97f1aa81ef1bebfe9
Mukunda-Katel <mukundakatel@gmail.com>
2025-11-19 21:08:16 +0545

    Task 2

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculator calc = new Calculator();
+
+            calc.PrintWelcome();
+
+            int sum = calc.Add(9, 6);
+            Console.WriteLine("Addition Result: " + sum);
+
+
+            int product1 = calc.Multiply(7, 5);
+            Console.WriteLine("Multiplication Result (7 * 5): " + product1);
+
+            int product2 = calc.Multiply(12);
+            Console.WriteLine("Multiplication Result (2 * default): " + product2);
+
+            Console.ReadLine();
+        }
+    }
+}
